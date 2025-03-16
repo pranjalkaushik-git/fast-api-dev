8f0c736746561b55c224d6b565d032c668953b5d
pranjalkaushik-git <officialpranjalkaushik@gmail.com>
2025-03-16 10:52:03 +0530

    Path parameters in get request

--- 1_fast_api_method_logic/books.py
+++ 1_fast_api_method_logic/books.py
@@ -13,6 +13,16 @@ BOOKS = [
     {'title': 'Title Six', 'author': 'Author Two', 'category': 'math'}
 ]
 
+#API endpoint to fetch all books
 @app.get('/books')
 async def get_all_books():
     return BOOKS
+
+
+#API endpoint to fetch speifici book by title
+@app.get('/books/{book_title}')
+async def get_book_by_title(book_title:str):
+    for book in BOOKS:
+        if book.get('title').casefold() == book_title.casefold():
+            return book
+    return {'message' : 'book not found'}
