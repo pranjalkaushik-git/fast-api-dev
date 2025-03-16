adcf8f5d749b06fbf7bced67d52ea31ea566a5d3
pranjalkaushik-git <officialpranjalkaushik@gmail.com>
2025-03-16 14:40:22 +0530

    query params completed

--- 1_fast_api_method_logic/books.py
+++ 1_fast_api_method_logic/books.py
@@ -19,10 +19,35 @@ async def get_all_books():
     return BOOKS
 
 
-#API endpoint to fetch speifici book by title
+#API endpoint to fetch specific book by title
 @app.get('/books/{book_title}')
 async def get_book_by_title(book_title:str):
     for book in BOOKS:
         if book.get('title').casefold() == book_title.casefold():
             return book
     return {'message' : 'book not found'}
+
+#API endpoint 
+
+@app.get('/books/')
+async def get_book_by_category(category:str):
+    BOOKS_TO_RETURN = []
+    for book in BOOKS:
+        if book.get('category').casefold() == category.casefold():
+            BOOKS_TO_RETURN.append(book)
+    if len(BOOKS_TO_RETURN) == 0:
+        return {'message' : 'no book found'}
+    else:
+        return BOOKS_TO_RETURN
+    
+
+#API endpoint to get books by author (path) and category (query)
+
+@app.get('/books/{author_name}/')
+async def get_book_by_author_category(author_name:str,category:str):
+    BOOKS_TO_RETURN = []
+    for book in BOOKS:
+        if book.get('author').casefold() == author_name.casefold():
+            if book.get('category').casefold() == category.casefold():
+                BOOKS_TO_RETURN.append(book)
+    return BOOKS_TO_RETURN
